60e98e685ccb011310c710fba45a80db9c26c62d
Rahmat Mustafa <66180950+mrahmatm@users.noreply.github.com>
2022-06-10 21:26:34 +0800

    letak bootstrap meow

--- index.php
+++ index.php
@@ -5,8 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <!-- bootstrap -->
+    <link href="bootstrap-5.2.0-beta1-dist/css/bootstrap.css" rel="stylesheet">
+    <script src="bootstrap/bootstrap-5.2.0-beta1-dist/js/bootstrap.bundle.js"></script>
 </head>
 <body>
-    
+    <h1>meow</h1>
 </body>
 </html>
