bee659024d4cab37c793952696bdebc141f4df7c
Aimaniac <aimanazha@gmail.com>
2022-06-17 23:13:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,11 +9,70 @@
     <?php
         include "common.php";
     ?>
+
+    <style>
+        body{
+            background-image: url("web.jpg");
+            background-color: #cccccc;
+        }
+        h1 {text-align: center;
+            margin-top: 50px;
+            font-family: 'Courier New', monospace;
+            font-size: 100px;
+        }
+        .container{
+            margin-left: 500px;
+        }
+        a.nav-link:link
+        {
+        color: #fb3f00;
+        text-decoration: none;
+        }
+        a.nav-link:visited
+        {
+        color: #fb3f00;
+        text-decoration: none;
+        }
+        a.nav-link:hover
+        {
+        color: #fb3f00;
+        text-decoration: none;
+        }
+        a.nav-link:active
+        {
+        color: #fb3f00;
+        text-decoration: none;
+        }    
+        button{
+
+            width: 150px;
+            height: 50px;
+            font-size: 20px;
+        }
+        img{
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+        }
+    </style>
 </head>
 <body>
+    
 
-    <h1>SMKBPJ</h1><br>
-    <a href="log in.php">go to log in page</a><br>
-    <a href="sign up.php">go to sign up page</a>
+
+    <h1>SMKBPJ</h1>
+    <img src="logo.png" alt="Logo Sekolah" width="150" height="200"  >
+     <div class="container">   
+    
+     
+    <button>
+     <a href="log in.php" class="nav-link">Log In
+        
+    </a></button>
+    
+    <button style="margin-left: 200px";>
+    <a href="sign up.php" class="nav-link">Sign Up</a>
+    </button>
+    </div> 
 </body>
 </html>
